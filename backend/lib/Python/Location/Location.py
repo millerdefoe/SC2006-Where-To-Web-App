f933fa8f8b7e2e5740e5c7830a3e684168716ed4
eventsni7per <mohamed_absar159@yahoo.com>
2025-03-23 20:35:46 +0800

    Added in gitignore for backend logs folder. Added in skeleton for DrivingRoute, Location and User classes

---
+++ backend/lib/Python/Location/Location.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+from lib.Python.Logging.PythonLogger import PythonLogger
+
+logger = PythonLogger(os.path.basename(__file__))
+
+class Location():
+
+    def __init__():
+
+        return True
+
+    def setPostalCode(postalCode: String):
+
+        return True
+
+    def convertPostalCodeToCoordinates():
+
+        return True
+
+    def setLatitudeLongitude(latitude: String, longitude: String):
+
+        return True
+
+    def getLocation():
+
+        return True
