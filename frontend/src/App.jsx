f6321504e58193c7d691d10e0bbdcc3d8c69d8f5
ANGELINAJDS <linasutiarto@gmail.com>
2025-03-25 23:23:26 +0800

    scaling and renaming of pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,53 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 import InputEndLocation from "./pages/InputEndLocation";
 import InputStartLocation from "./pages/InputStartLocation";
+import InputTPTMode from './pages/InputTPTMode';
+import FastestRoute from './pages/FastestRoute';
+import FastestRouteDirections from './pages/FastestRouteDirections'; 
+import LeastCongestedRoute from './pages/LeastCongestedRoute';
+import ViewLessCongestedRouteDirections from './pages/ViewLessCongestedRouteDirections';
 import ViewDrivingRoute from "./pages/ViewDrivingRoute";
-import SettingsPageSaveSuccessful from "./pages/SettingsPageSaveSuccessful";
+import DrivingRouteNav from './pages/DrivingRouteNav';
+import TPTRouteNav from './pages/TPTRouteNav';
+import ViewPublicTransportRoute from './pages/ViewPublicTransportRoute';
+import ViewCarParks from "./pages/ViewCarParks";
+import BookingFail from "./pages/BookingFail";
+import BookingSuccess from "./pages/BookingSuccess";
+import SettingsPage from "./pages/Settings";
+import SettingsSaved from "./pages/SettingsSaved";
+import SettingsFailed from './pages/SettingsFailed';
+import SettingsReset from './pages/SettingsReset';
+import Feedback from './pages/Feedback';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <Router>
       <div className="App"> 
         <Routes>
-          <Route path="/" element={<Navigate to="/view-driving-route" />} />
-          <Route path="/view-driving-route" element={<ViewDrivingRoute />} />
+          <Route path="/" element={<Navigate to="/end-location" />} />
           <Route path="/end-location" element={<InputEndLocation />} />
-          <Route path="/settings" element={<SettingsPageSaveSuccessful />} />
           <Route path="/start-location" element={<InputStartLocation />} />
+          <Route path="/input-transport-mode" element={<InputTPTMode />} />
+          <Route path="/fastest-route" element={<FastestRoute />} />
+          <Route path="/fastest-route-directions" element={<FastestRouteDirections />} />
+          <Route path="/least-congested-route" element={<LeastCongestedRoute />} />
+          <Route path="/view-less-congested-directions" element={<ViewLessCongestedRouteDirections />} />
+          <Route path="/public-transport-nav" element={<TPTRouteNav />} />
+          <Route path="/view-public-transport-route" element={<ViewPublicTransportRoute />} />
+          <Route path="/view-car-parks" element={<ViewCarParks />}/>
+          <Route path="/booking-fail" element={<BookingFail/>} />
+          <Route path="/booking-success" element={<BookingSuccess/>} />
+          <Route path="/view-driving-route" element={<ViewDrivingRoute />} />
+          <Route path="/driving-route-nav" element={<DrivingRouteNav />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/settings-saved" element={<SettingsSaved />} />
+          <Route path="/settings-failed" element={<SettingsFailed />} />
+          <Route path="/settings-reset" element={<SettingsReset />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
     </Router> 
@@ -24,3 +55,4 @@ function App() {
 }
 
 export default App;
+    
--- frontend/src/pages/ViewCarParks.jsx
+++ frontend/src/pages/ViewCarParks.jsx
@@ -1,25 +1,25 @@
 import React from "react";
+import { ReactComponent as Car } from "../assets/Car.svg";
 import SettingsButton from "../components/SettingsButton";
 import HomeButton from "../components/HomeButton";
 import NavBar from "../components/NavigationBar";
-import {ReactComponent as Car} from "../assets/Car.svg"; 
-import "../styles/ViewDrivingRoute.css";  
+import "../styles/ViewCarParks.css";
 
-const ViewDrivingRoute = () => {
-
-    return (
-      <div>
-        <HomeButton />
-        <SettingsButton />
+function ViewCarParks() {
+  return (
+    <div>
+      <SettingsButton/>
+      <HomeButton/>
+      <div className="wrapper-container">
         <NavBar />
-        <div className="transport-container">
+        <div className="mode-of-transport-container">
           <span className="transport-label">Mode of Transport:</span>
           <div className="car-icon-container">
             <Car className="car-icon" />
           </div>
         </div>
       </div>
-    );
-};
-
-export default ViewDrivingRoute;
+    </div>
+  );
+}
+export default ViewCarParks;
