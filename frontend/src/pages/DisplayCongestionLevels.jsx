8fa159d1f9cdeca119a5e6237c3e1bc79875288c
reneesim <164147928+reneesim@users.noreply.github.com>
2025-03-26 23:37:37 +0800

    view public transport route

---
+++ frontend/src/pages/DisplayCongestionLevels.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DisplayCongestionLevels() {
+  return (
+    <div>
+      <h1>Congestion Levels</h1>
+    </div>
+  );
+}
+
+export default DisplayCongestionLevels;
