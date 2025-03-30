504e73d3551700513e130a3c1c240577da07f0d7
eventsni7per <mohamed_absar159@yahoo.com>
2025-03-30 18:41:39 +0800

    removed database object from location file

--- backend/lib/Python/Location/Location.py
+++ backend/lib/Python/Location/Location.py
@@ -3,20 +3,8 @@ import json
 import os
 
 from lib.Python.Logging.PythonLogger import PythonLogger
-from lib.Python.Database.Database import Database
-from lib.Python.Helper.HelperFunctions import loadDatabaseCredentials
 
 logger = PythonLogger(os.path.basename(__file__))
-
-databaseCreds = loadDatabaseCredentials(r".\creds\priv\database.json")
-
-dbObj = Database(
-    databaseCreds["host"],
-    databaseCreds["database"],
-    databaseCreds["username"],
-    databaseCreds["password"],
-    databaseCreds["port"]
-)
 
 class Location(): 
     def __init__(self, latitude, longitude):
