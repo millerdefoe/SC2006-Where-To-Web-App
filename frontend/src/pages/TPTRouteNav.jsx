159901f52892494dee1d32f0b1f4bd2355800db8
millerdefoe <spyridon2001@gmail.com>
2025-04-06 17:09:15 +0800

    Update TPTRouteNav.jsx
    Connected Map API

--- frontend/src/pages/TPTRouteNav.jsx
+++ frontend/src/pages/TPTRouteNav.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { useLocation } from "react-router-dom";
 import { ReactComponent as Bus } from "../assets/Bus.svg";
-import { ReactComponent as Train } from "../assets/Train.svg";
-import { ReactComponent as Walking } from "../assets/Walking.svg";
-import { ReactComponent as TransportArrow } from "../assets/TransportArrow.svg";
 import SettingsButton from "../components/SettingsButton";
 import HomeButton from "../components/HomeButton";
 import NavBar from "../components/NavigationBar";
 import ModeOfTransport from "../components/ModeOfTransport";
 import EndButton from '../components/EndButton';
-import mapImage from "../assets/inputStartLocationMap.png";
 import DirectionDescription from '../components/DirectionDescription';
+import MapWithRoute from "../components/MapDrivingRoute"; // ✅ Import the map
 import "../styles/TPTRouteNav.css";
 import "../styles/common.css";
 
@@ -27,18 +24,19 @@ function TPTRouteNav() {
 
       <div className="leftContainer3">
         <div className="map-container4">
-          <img src={mapImage} alt="Map" className="map-image4" />
+          <MapWithRoute
+            encodedPolyline={routeData?.polyline?.encodedPolyline}
+            mapContainerClassName="map-image4"
+          />
           <div className="endButton-container">
-            <EndButton/>
+            <EndButton />
           </div>
         </div>
       </div>
 
       <div className="rightContainer3">
         <div className="routeNav-container">
-          <DirectionDescription
-            routeData={routeData}
-          />
+          <DirectionDescription routeData={routeData} />
         </div>
       </div>
     </div>
